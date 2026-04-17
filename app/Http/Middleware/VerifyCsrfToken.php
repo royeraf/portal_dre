5c0621804b560a4dad394c0d07db8adede9d16e7
royer <royercfm@gmail.com>
2026-04-17 11:56:19 -0500

    chore: add login route to CSRF exception list for debugging purposes

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        // Temporal: diagnosticar si el problema es CSRF o sesión post-login
+        'login',
     ];
 }
